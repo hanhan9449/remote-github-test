3b53f4717fbac2167428072cf03cb5066c46d89d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-03-25 13:19:59 +0000

    Deploy hanhan9449/remote-github-test to github.com/hanhan9449/remote-github-test.git:gh-pages

---
+++ precache-manifest.3980bf3008b4a4e62a7857d726278a9b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db0a0963a8c4633b5f09b4d4dd2d181b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7173bc301b2318aecfc6",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0cc2a2e4e5e4a7098b83",
+    "url": "/static/js/2.ae23bf2b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ae23bf2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7173bc301b2318aecfc6",
+    "url": "/static/js/main.bf5ceb51.chunk.js"
+  },
+  {
+    "revision": "0b672acbacb8931c81b7",
+    "url": "/static/js/runtime-main.fb1aa516.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
